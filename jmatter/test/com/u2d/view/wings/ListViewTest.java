7d48843b5ab927ef0c0680aa90ffef12740ab2c8
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2006-08-18 05:09:47 +0000

    nothing

---
+++ jmatter/test/com/u2d/view/wings/ListViewTest.java
@@ -0,0 +1,14 @@
+package com.u2d.view.wings;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: eitan
+ * Date: Aug 16, 2006
+ * Time: 9:35:12 PM
+ */
+public class ListViewTest
+{
+   public static void main(String[] args)
+   {
+   }
+}
