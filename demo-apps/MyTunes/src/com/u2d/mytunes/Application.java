c0ddf546b9383e603cbba0512ed5d1b4829436cf
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2006-09-07 04:17:42 +0000

    now supporting optional loading of data in json format (as alternative to xml format)

--- demo-apps/MyTunes/src/com/u2d/mytunes/Application.java
+++ demo-apps/MyTunes/src/com/u2d/mytunes/Application.java
@@ -13,7 +13,8 @@ public class Application extends com.u2d.app.Application
    public void initObjects()
    {
       super.initObjects();
-      CodesList.populateItemsFor(Genre.class,  "genres.xml");
+//      CodesList.populateItemsFor(Genre.class,  "genres.xml");
+      CodesList.populateItemsFor(Genre.class,  "genres.json");
    }
 
    public static void main(String[] args)
