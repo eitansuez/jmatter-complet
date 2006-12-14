d9d112d721191b295b51ddd107561ffa83d506a2
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2006-12-14 16:58:33 +0000

    integrating intellij idea tooling into project

---
+++ jmatter/tools/IDEA/ACO.java
@@ -0,0 +1,16 @@
+package ${PACKAGE_NAME};
+
+import com.u2d.model.AbstractComplexEObject;
+import com.u2d.model.Title;
+import com.u2d.type.atom.StringEO;
+
+public class ${NAME} extends AbstractComplexEObject
+{
+   private final StringEO name = new StringEO();
+
+   public ${NAME}() {}
+
+   public StringEO getName() { return name; }
+   
+   public Title title() { return name.title(); }
+}
