30871f129fb9a84a45da0ca20928bd98f61602a7
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2007-02-12 20:01:11 +0000

    removed inner class, no longer necessary

--- modules/swingvm/src/com/u2d/view/swing/OutlookFolderView.java
+++ modules/swingvm/src/com/u2d/view/swing/OutlookFolderView.java
@@ -1,22 +1,13 @@
 package com.u2d.view.swing;
 
 import com.u2d.view.ComplexEView;
-import com.u2d.view.EView;
-import com.u2d.view.swing.dnd.BasicTransferHandler;
 import com.u2d.view.swing.list.JListView;
 import com.u2d.type.composite.Folder;
 import com.u2d.model.EObject;
 import com.u2d.model.ComplexEObject;
-import com.u2d.model.AbstractListEO;
-import com.u2d.field.ListItemAssociation;
-import com.u2d.field.Association;
 import com.l2fprod.common.swing.JOutlookBar;
-import com.l2fprod.common.swing.PercentLayout;
-import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.event.ChangeEvent;
-import javax.swing.event.ListDataListener;
-import javax.swing.event.ListDataEvent;
 import java.beans.PropertyChangeEvent;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
@@ -82,108 +73,6 @@ public class OutlookFolderView extends JOutlookBar implements ComplexEView
       }
       _tabs.clear();
       removeAll();
-   }
-
-
-   class VerticalFolderPane extends FolderPanel
-                            implements ListDataListener
-   {
-      VerticalFolderPane(Folder folder)
-      {
-         super(folder);
-         setLayout(new PercentLayout(PercentLayout.VERTICAL, 0));
-         setOpaque(false);
-         folder.getItems().addListDataListener(this);
-
-         ComplexEObject item;
-         for (int i=0; i<folder.size(); i++)
-         {
-            item = (ComplexEObject) folder.get(i);
-            add(getViewForItem(folder, item));
-         }
-      }
-      
-      private JComponent getViewForItem(Folder folder, ComplexEObject item)
-      {
-         EView view = item.getIconView();
-         Association association = folder.association("items");
-         ListItemAssociation liassociation = new ListItemAssociation(association, item);
-         TransferHandler transferHandler = new BasicTransferHandler(view, liassociation);
-         JComponent comp = (JComponent) view;
-         comp.setTransferHandler(transferHandler);
-         return comp;
-      }
-      
-      public void detach()
-      {
-         _folder.getItems().removeListDataListener(this);
-         for (int i=0; i<getComponentCount(); i++)
-         {
-            ((EView) getComponent(i)).detach();
-         }
-         removeAll();
-      }
-      
-      public void intervalAdded(final ListDataEvent e)
-      {
-         SwingUtilities.invokeLater(new Runnable()
-         {
-            public void run()
-            {
-               AbstractListEO source = (AbstractListEO) e.getSource();
-               ComplexEObject eo = null;
-               for (int i=e.getIndex0(); i<=e.getIndex1(); i++)
-               {
-                  eo = (ComplexEObject) source.getElementAt(i);
-                  add(getViewForItem(_folder, eo), i);
-               }
-
-               revalidate(); repaint();
-            }
-         });
-      }
-
-      public void intervalRemoved(final ListDataEvent e)
-      {
-         SwingUtilities.invokeLater(new Runnable()
-         {
-            public void run()
-            {
-               for (int i=e.getIndex0(); i<=e.getIndex1(); i++)
-               {
-                  ((EView) getComponent(i)).detach();
-                  remove(i);
-               }
-
-               revalidate(); repaint();
-            }
-         });
-      }
-
-      public void contentsChanged(final ListDataEvent e)
-      {
-         SwingUtilities.invokeLater(new Runnable()
-         {
-            public void run()
-            {
-               for (int i=0; i<getComponentCount(); i++)
-                  ((EView) getComponent(i)).detach();
-               removeAll();
-
-               AbstractListEO source = (AbstractListEO) e.getSource();
-
-               ComplexEObject eo = null;
-               for (int i=0; i<source.getSize(); i++)
-               {
-                  eo = (ComplexEObject) source.getElementAt(i);
-                  add(getViewForItem(_folder, eo));
-               }
-
-               revalidate(); repaint();
-            }
-         });
-
-      }
    }
 
    public EObject getEObject() { return _folder; }
