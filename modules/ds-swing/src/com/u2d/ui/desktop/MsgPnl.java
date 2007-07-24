ce5880626e55226a818a9fb788077356fae5293e
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2007-07-24 19:02:06 +0000

    updated css4swing (now supports opacity property on JXPanel's) so that now styling of MsgPnl can be specified entirely in CSS, simplifying the implementation of ds-swing's MsgPnl

--- modules/ds-swing/src/com/u2d/ui/desktop/MsgPnl.java
+++ modules/ds-swing/src/com/u2d/ui/desktop/MsgPnl.java
@@ -3,12 +3,9 @@
  */
 package com.u2d.ui.desktop;
 
-import org.jdesktop.swingx.painter.AbstractPainter;
-import org.jdesktop.swingx.painter.Painter;
 import org.jdesktop.swingx.JXPanel;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.RoundRectangle2D;
 import java.awt.event.*;
 import com.u2d.ui.UIUtils;
 import com.u2d.css4swing.style.ComponentStyle;
@@ -18,15 +15,12 @@ import com.u2d.css4swing.style.ComponentStyle;
  */
 public class MsgPnl extends JXPanel
 {
-   private static Painter backgroundPainter = new MsgPanelPainter();
-   
    private JLabel _label;
    private static int _delay = 2000; // ms
    private Timer _timer;
    
    public MsgPnl()
    {
-      setBackgroundPainter(backgroundPainter);
       ComponentStyle.addClass(this, "feedback-pane");
       setVisible(false);
       
@@ -71,36 +65,4 @@ public class MsgPnl extends JXPanel
       _label.setText("");
       setVisible(false);
    }
-
-   static class MsgPanelPainter extends AbstractPainter
-   {
-      public MsgPanelPainter()
-      {
-         super();
-         setUseCache(false);
-         setAntialiasing(RenderingHints.VALUE_ANTIALIAS_ON);
-      }
-
-      public void paintBackground(Graphics2D g2, JComponent c)
-      {
-         Composite composite = g2.getComposite();
-         Color color = g2.getColor();
-
-         float alpha = 0.7f;
-         if (composite instanceof AlphaComposite)
-         {
-            alpha *= ((AlphaComposite) composite).getAlpha();
-         }
-         
-         g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-         g2.setColor(c.getBackground());
-         RoundRectangle2D rect = new RoundRectangle2D.Double(0, 0, c.getWidth() - 1, c.getHeight() - 1, 24, 24);
-         g2.fill(rect);
-
-         
-         g2.setColor(color);
-         g2.setComposite(composite);
-      }
-   }
-   
 }
