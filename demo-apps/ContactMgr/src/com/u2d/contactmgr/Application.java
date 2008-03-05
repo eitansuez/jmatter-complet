f7d824dcd2f64ed2b0928a32cf535a77e54cd391
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2008-03-05 18:49:42 +0000

    illustrating how to add new items to visual command interface index upon creation

--- demo-apps/ContactMgr/src/com/u2d/contactmgr/Application.java
+++ demo-apps/ContactMgr/src/com/u2d/contactmgr/Application.java
@@ -1,6 +1,11 @@
 package com.u2d.contactmgr;
 
 import com.u2d.model.AbstractListEO;
+import com.u2d.model.ComplexType;
+import com.u2d.model.ComplexEObject;
+import com.u2d.pubsub.AppEventType;
+import com.u2d.pubsub.AppEventListener;
+import com.u2d.pubsub.AppEvent;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,5 +20,14 @@ public class Application extends com.u2d.app.Application
       super.postInitialize();
       AbstractListEO bookmarks = getPersistenceMechanism().list(PersonContact.class);
       contributeToIndex(bookmarks);
+      
+      ComplexType.forClass(PersonContact.class).addAppEventListener(AppEventType.CREATE, 
+              new AppEventListener()
+              {
+                 public void onEvent(AppEvent evt)
+                 {
+                    contributeToIndex((ComplexEObject) evt.getEventInfo());
+                 }
+              });
    }
 }
