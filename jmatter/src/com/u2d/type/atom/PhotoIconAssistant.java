95a8fd56ebcc152fbf66c41a98720738473eaaae
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2008-03-06 18:54:11 +0000

    minor:  made photoiconassistance.scale more generic

--- jmatter/src/com/u2d/type/atom/PhotoIconAssistant.java
+++ jmatter/src/com/u2d/type/atom/PhotoIconAssistant.java
@@ -5,8 +5,6 @@ package com.u2d.type.atom;
 
 import com.u2d.model.ComplexEObject;
 import com.u2d.model.IconLoader;
-import com.u2d.model.ComplexType;
-
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import java.awt.*;
@@ -80,13 +78,15 @@ public class PhotoIconAssistant
       return new ImageIcon(scaledImg);
    }
       
-   public static Image scale(ImageIcon icon, int w, int h)
+   public static Image scale(Icon icon, int w, int h)
    {
       int h1 = icon.getIconHeight();
       int w1 = icon.getIconWidth();
+
+      Image img = getImage(icon);
+
       double ratio = (double) h1 / w1;
       double desiredRatio = (double) h / w;
-      Image img = icon.getImage();
       if (ratio > desiredRatio)  // trim height
       {
          int h2 = (int) (w1 * desiredRatio);
@@ -102,7 +102,21 @@ public class PhotoIconAssistant
       
       return img.getScaledInstance(w, h, Image.SCALE_FAST);
    }
-   
+   private static Image getImage(Icon icon)
+   {
+      if (icon instanceof ImageIcon)
+      {
+         return ((ImageIcon) icon).getImage();
+      }
+      else
+      {
+         Image img = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+         Graphics g = img.getGraphics();
+         icon.paintIcon(null, g, 0, 0);
+         return img;
+      }
+   }
+
    private static Image crop(Image img, int x, int y, int w, int h)
    {
       ImageFilter filter = new CropImageFilter(x, y, w, h);
