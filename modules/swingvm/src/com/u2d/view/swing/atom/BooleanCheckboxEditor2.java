45a454709788c4591d9cbf30ca81f7a1b70d7823
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2008-04-17 21:06:30 +0000

    added two new variant editors for boolean types

---
+++ modules/swingvm/src/com/u2d/view/swing/atom/BooleanCheckboxEditor2.java
@@ -0,0 +1,37 @@
+package com.u2d.view.swing.atom;
+
+import com.u2d.model.AtomicEditor;
+import com.u2d.model.AtomicEObject;
+import com.u2d.view.ActionNotifier;
+import com.u2d.type.atom.BooleanEO;
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Eitan Suez
+ * Date: Apr 17, 2008
+ * Time: 10:13:05 AM
+ */
+public class BooleanCheckboxEditor2 extends JCheckBox implements AtomicEditor, ActionNotifier
+{
+   public BooleanCheckboxEditor2()
+   {
+      setOpaque(false);
+   }
+
+   public int bind(AtomicEObject value)
+   {
+      BooleanEO eo = (BooleanEO) value;
+      eo.setValue(isSelected());
+      return 0;
+   }
+
+   public void render(AtomicEObject value)
+   {
+      BooleanEO eo = (BooleanEO) value;
+      setText(eo.field().label());
+      setSelected(eo.booleanValue());
+   }
+
+   public void passivate() { }
+}
