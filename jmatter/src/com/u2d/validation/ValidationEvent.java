22ee60bb03b5701b8c24975a3cda16b058edd607
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2008-05-13 21:54:50 +0000

    added tostring method to validationevent (convenience);  defined colors for invalid textcomponent fields

--- jmatter/src/com/u2d/validation/ValidationEvent.java
+++ jmatter/src/com/u2d/validation/ValidationEvent.java
@@ -4,6 +4,7 @@
 package com.u2d.validation;
 
 import java.util.EventObject;
+import java.awt.*;
 
 /**
  * @author Eitan Suez
@@ -12,7 +13,11 @@ public class ValidationEvent extends EventObject
 {
    private String _msg;
    private boolean _statusType = false;
-   
+
+   public static final Color INVALID_COLOR = new Color(0xFFD7D7);
+   public static final Color REQUIRED_COLOR = new Color(0xEBEBFF);
+   public static final Color NORMAL_COLOR = Color.white;
+
    public ValidationEvent(Object source, String msg, boolean statusType)
    {
       super(source);
@@ -28,4 +33,8 @@ public class ValidationEvent extends EventObject
    public String getMsg()    { return _msg; }
    public boolean isStatusMsg() { return _statusType; }
 
+   public String toString()
+   {
+      return String.format("Validation Event [msg='%s', on source object: '%s']", _msg, getSource());
+   }
 }
