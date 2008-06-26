f3c39379db637465bbb3319e4ace6f8ec03809cc
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2008-06-26 22:06:36 +0000

    adding address to hotel; to demonstrate looking up an address on a map

--- demo-apps/Sympster/src/com/u2d/sympster/Hotel.java
+++ demo-apps/Sympster/src/com/u2d/sympster/Hotel.java
@@ -1,9 +1,20 @@
 package com.u2d.sympster;
 
 import com.u2d.persist.Persist;
+import com.u2d.type.composite.USAddress;
 
 @Persist
 public class Hotel extends Venue
 {
    public Hotel() {}
+
+   public void initialize()
+   {
+      super.initialize();
+      address.initialize();
+   }
+
+   private final USAddress address = new USAddress();
+   public USAddress getAddress() { return address; }
+
 }
