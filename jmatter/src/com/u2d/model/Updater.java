a25c7b9e3f53961a12c973e704c307313288379a
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2008-11-03 20:15:31 +0000

    should have been included in last commit..

---
+++ jmatter/src/com/u2d/model/Updater.java
@@ -0,0 +1,12 @@
+package com.u2d.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Eitan Suez
+ * Date: Oct 31, 2008
+ * Time: 3:27:56 PM
+ */
+public interface Updater
+{
+   public EObject update();
+}
