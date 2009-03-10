95303988f7cfff31bb64c2f4676aa84aec446625
eitan <eitan@51fe0b4d-db0b-db11-8033-0007e9470735>
2009-03-10 19:56:37 +0000

    making abeilleform extend formpane to inherit scrollable implementation

--- modules/swingvm/src/com/u2d/view/swing/AbeilleForm.java
+++ modules/swingvm/src/com/u2d/view/swing/AbeilleForm.java
@@ -20,7 +20,7 @@ import java.io.File;
  * Date: Apr 29, 2008
  * Time: 12:36:48 PM
  */
-public abstract class AbeilleForm extends JPanel implements IFormView
+public abstract class AbeilleForm extends FormPane implements IFormView
 {
    protected java.util.List<EView> _views = new ArrayList<EView>();
 
@@ -101,7 +101,11 @@ public abstract class AbeilleForm extends JPanel implements IFormView
 
       return count;
    }
-   public int validateValue() { return 0; }
+
+   public int validateValue()
+   {
+      return eo().validate();
+   }
 
    public boolean isMinimized() { return false; }
 
